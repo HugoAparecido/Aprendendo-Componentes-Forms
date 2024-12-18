f3bb20fffa7dd8fe3d4d5f34ce8868308d4d0a89
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2024-12-18 17:11:08 -0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,41 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            textBox1 = new TextBox();
+            btn_adicionar = new Button();
+            tb_veiculo = new TextBox();
+            label1 = new Label();
+            tb_listaVeiculos = new TextBox();
             SuspendLayout();
             // 
-            // button1
+            // btn_adicionar
             // 
-            button1.Location = new Point(12, 45);
-            button1.Name = "button1";
-            button1.Size = new Size(125, 29);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btn_adicionar.Location = new Point(211, 30);
+            btn_adicionar.Name = "btn_adicionar";
+            btn_adicionar.Size = new Size(125, 29);
+            btn_adicionar.TabIndex = 1;
+            btn_adicionar.Text = "Adicionar";
+            btn_adicionar.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // tb_veiculo
             // 
-            textBox1.Location = new Point(12, 12);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 2;
+            tb_veiculo.Location = new Point(12, 32);
+            tb_veiculo.Name = "tb_veiculo";
+            tb_veiculo.Size = new Size(193, 27);
+            tb_veiculo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Digite um Veículo";
+            // 
+            // tb_listaVeiculos
+            // 
+            tb_listaVeiculos.Location = new Point(12, 66);
+            tb_listaVeiculos.Multiline = true;
+            tb_listaVeiculos.Name = "tb_listaVeiculos";
+            tb_listaVeiculos.Size = new Size(324, 315);
+            tb_listaVeiculos.TabIndex = 4;
             // 
             // F_Principal
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
+            ClientSize = new Size(348, 399);
+            Controls.Add(tb_listaVeiculos);
+            Controls.Add(label1);
+            Controls.Add(tb_veiculo);
+            Controls.Add(btn_adicionar);
             Name = "F_Principal";
-            Text = "Componentes - CFB";
+            Text = "Componentes - CFB Cursos";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private Button button1;
-        private TextBox textBox1;
+        private Button btn_adicionar;
+        private TextBox tb_veiculo;
+        private Label label1;
+        private TextBox tb_listaVeiculos;
     }
 }
